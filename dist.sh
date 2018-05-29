eba8d5092839fffa5cdb13fbd3099903ff5eedc9
bandesz <bandesz@gmail.com>
2018-05-29 14:30:36 +0100

    Add -ldflags '-s -w' to go build to strip debugging information from final binaries

--- dist.sh
+++ dist.sh
@@ -10,7 +10,7 @@ for arch in ${ALL_GOARCH}; do
   for platform in ${ALL_GOOS}; do
     file="bin/${NAME}-${version}.${platform}.${arch}"
     echo "Building ${file}"
-    CGO_ENABLED=0 GOOS=${platform} GOARCH=${arch} ${GOBUILD} -o ${file}
+    CGO_ENABLED=0 GOOS=${platform} GOARCH=${arch} ${GOBUILD} -ldflags="-s -w" -o ${file}
   done
 done
 
